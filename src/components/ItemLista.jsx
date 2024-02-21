d9129681544e4d42fd337decb25c58c3019d8a90
ISTIC <istic@info.local>
2024-02-21 10:33:22 -0300

    fundo verde

--- src/components/ItemLista.jsx
+++ src/components/ItemLista.jsx
@@ -1,10 +1,11 @@
 import { Icon } from '@iconify/react';
 import style from './ItemLista.module.css';
+import { useState } from 'react';
 
 function ItemLista(props) {
     return (
         <div className={style.section_item_lista}>
-            <input id='checkbox' type="checkbox" />
+            <input type="checkbox"/>
             <p>{props.text}</p>
             <Icon className={style.icone_lixo} icon="carbon:trash-can" />
         </div>
